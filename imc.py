817395756dc30225e640e8e6bf9719aaa36004af
dvpinedo <dv.jpinedo@gmail.com>
2022-11-29 17:10:01 -0500

    imc

---
+++ imc.py
@@ -0,0 +1,5 @@
+
+peso = input("Ingrese su peso en kg: ")
+estatura = input("Ingrese su estatura en metros: ")
+imcs = round (float(peso)/float(estatura)**2,2)
+print("Tu indice de masa corporal es " + str(imcs))
